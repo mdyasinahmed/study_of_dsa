222378b537951020de4eddd75e227addb9f4255f
mdyasinahmed <yasinahmed7875@gmail.com>
2024-04-25 01:15:04 +0600

    add evenOdd finding code with function

---
+++ function/bootcamp/solve/evenOdd.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isEven(int x){
+    if(x&1) {
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(isEven(n)){
+        cout << "Even";
+    } else {
+        cout << "Odd";
+    }
+
+    return 0;
+}

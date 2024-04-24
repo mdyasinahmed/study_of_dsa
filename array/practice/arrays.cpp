06cf26c045a43a455c8ff6f5742f637095b12eb4
mdyasinahmed <yasinahmed7875@gmail.com>
2024-04-24 12:55:24 +0600

    add array practice code

---
+++ array/practice/arrays.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int arr[5] = {1, 3, 5, 7, 9};
+
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    cout << len << endl;
+
+
+    return 0;
+}
